ec272ce58f717097d717911f62255b8789d0be1a
Emily Switzer <emilyswitzer90@gmail.com>
2022-04-14 13:42:24 +0100

    updated app

--- app.py
+++ app.py
@@ -1,39 +1,61 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 import os
-from datetime import datetime
+
 app = Flask(__name__)
 
-data = {
-    "drinks": [
-        {
-            "name": "Grape", 
-            "description": "Delicious grape fruit drink",
-            "date": datetime.now()
-            },
-            {
-            "name": "Lemon", 
-            "description": "Undiluted lemon fruit drink",
-            "date": datetime.now()
-            },
-            {
-            "name": "Mango", 
-            "description": "This is a mango fruit",
-            "date": datetime.now()
-            }
-        
-    ]
-} 
+# /// = relative path, //// = absolute path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
 
 @app.route("/")
-def index():
-        return "Welcome To My Drinks API"
+def home():
+    todo_list = Todo.query.all()
+    return render_template("base.html", todo_list=todo_list)
 
-@app.route('/drinks')
-def get_drinks():
-    return data
+
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+
+
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
 
 
 if __name__ == "__main__":
-    app.debug = False
+    db.create_all()
     port = int(os.environ.get("PORT", 5000));
-    app.run(host="0.0.0.0", port=port);
+    app.run(host="0.0.0.0", debug=False)
+
--- tests/route_test.py
+++ tests/route_test.py
@@ -1,13 +1,27 @@
 import json
 from app import app
 
-def test_get_all_drinks():
-    response = app.test_client().get('/drinks')
-    res = json.loads(response.data.decode('utf-8')).get("drinks")
-    assert type(res[0]) is dict
-    assert type(res[1]) is dict
-    assert res[0]['name'] == 'Grape'
-    assert res[1]['name'] == 'Lemon'
+def test_home_route():
+    response = app.test_client().get('/')
     assert response.status_code == 200
-    assert type(res) is list
-    ...
+    assert b'<h1>Todo List</h1>' in response.data
+
+
+def test_post_route():
+    response = app.test_client().post('/add', data=dict(title="Test"))
+    assert response.status_code == 200
+    assert b'<h1>Todo List</h1>' in response.data
+    assert b'Test' in response.data 
+    
+
+def test_update_route():
+    response = app.test_client().post('/update/1')
+    assert response.status_code == 200
+    assert b'<h1>Todo List</h1>' in response.data
+    assert b'Test' in response.data 
+    
+def test_delete():
+    response = app.test_client().post('/delete/1')
+    assert response.status_code == 200
+    assert b'<h1>Todo List</h1>' in response.data
+    assert b'Test' in response.data
